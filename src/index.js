5b8b6e2714235ccd2eff99f89fbeb4eb01453345
shubham-dreamer <gns7161356@gmail.com>
2021-03-28 21:38:35 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  ,
+
   document.getElementById('root')
 );
 
@@ -15,3 +18,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
